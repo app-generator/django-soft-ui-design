e7813f0e9b1c074c5b813785d6e5413fc683b846
Viktoria Pavlej <viktorialondon16@gmail.com>
2022-11-15 00:43:10 +0000

    Add Worker model as AbstractUser

--- apps/home/models.py
+++ apps/home/models.py
@@ -4,7 +4,55 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
+from django.urls import reverse
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE, default=1)
+
+    class Meta:
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    def get_absolute_url(self):
+        return reverse("home:worker-detail", kwargs={'pk': self.pk})
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+
+    # PRIORITY_CHOICES = {
+    #     ("1", "Urgent"),
+    #     ("2", "High"),
+    #     ("3", "Medium"),
+    #     ("4", "Low")
+    # }
+
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+    deadline = models.DateField(blank=True)
+    is_completed = models.BooleanField(default=False)
+    #priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES, default="4")
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker, related_name="tasks")
+
+    def __str__(self):
+        return self.name
 
