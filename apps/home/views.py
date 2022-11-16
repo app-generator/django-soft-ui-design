6c4579691d747afd0f32712df5a0bfbb4723c23e
Viktoria Pavlej <viktorialondon16@gmail.com>
2022-11-16 22:07:09 +0000

    Add task list to worker detail page

--- apps/home/views.py
+++ apps/home/views.py
@@ -12,6 +12,7 @@ from django.template import loader
 from django.urls import reverse, reverse_lazy
 from django.views import generic
 
+from apps.home.forms import WorkerCreationForm
 from apps.home.models import Worker, Task, TaskType
 
 
@@ -77,12 +78,7 @@ class WorkerDetailView(LoginRequiredMixin, generic.DetailView):
 class WorkerCreateView(LoginRequiredMixin, generic.CreateView):
     model = Worker
     form_class = WorkerCreationForm
-    success_url = reverse_lazy("tasks:worker-list")
-
-
-class DriverDeleteView(LoginRequiredMixin, generic.DeleteView):
-    model = Worker
-    success_url = reverse_lazy("tasks:worker-list")
+    success_url = reverse_lazy("home:worker-list")
 
 
 @login_required
@@ -90,3 +86,12 @@ def delete_worker(request, pk):
     worker = Worker.objects.get(id=pk)
     worker.delete()
     return redirect("home:worker-list")
+
+
+@login_required
+def change_task_status(request, pk1, pk2):
+    task = Task.objects.get(id=pk1)
+    task.is_completed = not task.is_completed
+    task.save()
+    return redirect(reverse("home:worker-detail", kwargs={'pk': pk2}))
+
