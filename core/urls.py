d3d6ea4b604c2ea07c725f6c98f5284fbfefc937
Viktoria Pavlej <viktorialondon16@gmail.com>
2022-11-17 12:01:04 +0000

    Add Task list page

--- core/urls.py
+++ core/urls.py
@@ -7,13 +7,11 @@ from django.contrib import admin
 from django.urls import path, include  # add this
 
 urlpatterns = [
-    path('admin/', admin.site.urls),          # Django admin route
-    path("", include("apps.authentication.urls")), # Auth routes - login / register
+    path('admin/', admin.site.urls),  # Django admin route
+    path("", include("apps.authentication.urls")),  # Auth routes - login / register
 
     # ADD NEW Routes HERE
 
     # Leave `Home.Urls` as last the last line
-    path("", include(("apps.home.urls",  "apps.home"), namespace="home")),
+    path("", include(("apps.home.urls", "apps.home"), namespace="home")),
 ]
-
-
