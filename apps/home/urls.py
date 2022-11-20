7dc3163e477d88d2475c1209b788fa1ae2bf9061
Viktoria Pavlej <viktorialondon16@gmail.com>
2022-11-20 22:44:57 +0000

    Fix logout redirect page

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -6,12 +6,12 @@ Copyright (c) 2019 - present AppSeed.us
 from django.urls import path, re_path
 from apps.home import views
 from apps.home.views import WorkerListView, WorkerDetailView, delete_worker, WorkerCreateView, change_task_status, \
-    TaskListView, TaskDetailView, TaskCreateView, assign_to_task
+    TaskListView, TaskDetailView, TaskCreateView, assign_to_task, TaskUpdateView, delete_task
 
 urlpatterns = [
 
     # The home page
-    path('', views.index, name='home'),
+    path('', views.index, name="home"),
 
     path("workers/", WorkerListView.as_view(), name="worker-list"),
     path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
@@ -20,9 +20,11 @@ urlpatterns = [
 
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
-    # path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("tasks/status/<int:pk1>/<int:pk2>/", change_task_status, name="change-task-status"),
     path("tasks/assign/<int:pk>/", assign_to_task, name="assign-to-task"),
+    path("tasks/delete/<int:pk>/", delete_task, name="task-delete"),
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
 
