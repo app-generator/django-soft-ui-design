fe9123ce050c17af38df176f3ec292d96674ea28
Viktoria Pavlej <viktorialondon16@gmail.com>
2022-11-23 22:52:34 +0000

    Add delete and update buttons to task type list

---
+++ apps/home/forms.py
@@ -0,0 +1,69 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import SelectDateWidget
+
+from apps.home.models import Worker, Task, TaskType, Position
+
+
+class WorkerCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm.Meta):
+        model = Worker
+        fields = UserCreationForm.Meta.fields + ("first_name", "last_name", "position",)
+
+
+class TaskCreationForm(forms.ModelForm):
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+    deadline = forms.DateField(widget=SelectDateWidget(empty_label=("year", "month", "day")), required=False)
+
+    class Meta:
+        model = Task
+        fields = "__all__"
+
+
+class TaskUpdateForm(forms.ModelForm):
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+    deadline = forms.DateField(widget=SelectDateWidget(empty_label=("year", "month", "day")), required=False)
+
+    class Meta:
+        model = Task
+        fields = "__all__"
+
+
+class TaskSearchForm(forms.Form):
+    task_type = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by task type..."})
+    )
+
+
+class TaskTypeCreationForm(forms.ModelForm):
+
+    class Meta:
+        model = TaskType
+        fields = "__all__"
+
+
+class TaskTypeUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = TaskType
+        fields = "__all__"
+
+
+class PositionCreationForm(forms.ModelForm):
+
+    class Meta:
+        model = Position
+        fields = "__all__"
---
+++ apps/home/templatetags/query_transform.py
@@ -0,0 +1,15 @@
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag
+def query_transform(request, **kwargs):
+    updated = request.GET.copy()
+    for k, v in kwargs.items():
+        if v is not None:
+            updated[k] = v
+        else:
+            updated.pop(k, 0)
+
+    return updated.urlencode()
