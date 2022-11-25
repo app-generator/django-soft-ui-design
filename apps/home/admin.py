9884f80605bcdeb165fb9e9a23b273295e3b501f
Viktoria Pavlej <viktorialondon16@gmail.com>
2022-11-25 05:40:32 +0000

    Create tests for admin

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,8 +1,3 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,8 +1,3 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from django.urls import path, re_path
 from apps.home import views
 from apps.home.views import (
@@ -28,7 +23,6 @@ from apps.home.views import (
 )
 
 urlpatterns = [
-    # The home page
     path("", views.index, name="home"),
     path("workers/", WorkerListView.as_view(), name="worker-list"),
     path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
