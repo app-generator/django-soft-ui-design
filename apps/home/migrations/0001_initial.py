e2e7d7d6b84004fc7b321fe1355d945781864c91
Viktoria Pavlej <viktorialondon16@gmail.com>
2022-12-01 00:09:38 +0000

    Add images of workers in task list

--- apps/home/migrations/0001_initial.py
+++ apps/home/migrations/0001_initial.py
@@ -89,7 +89,7 @@ class Migration(migrations.Migration):
                     "is_active",
                     models.BooleanField(
                         default=True,
-                        help_text="Designates whether this user should be treated as active. Unselect this instead of deleting accounts.",
+                        help_text="Designates whether this user should be treated as active. Unselect this instead of deleting register.",
                         verbose_name="active",
                     ),
                 ),
