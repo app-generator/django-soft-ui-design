1ae5fabce6df0a24297a0de5678945d810bdb590
Viktoria Pavlej <viktorialondon16@gmail.com>
2022-12-05 12:35:56 +0000

    Add url to serve media files

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,11 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
+import django
 from django.conf import settings
+from django.conf.urls import url
 from django.contrib import admin
+
 from django.urls import path, include  # add this
 from django.conf.urls.static import static
 
@@ -14,6 +17,9 @@ urlpatterns = [
 urlpatterns += static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
 )  # Media files routes
+urlpatterns += [
+    url(r'^media/(?P<path>.*)$', django.views.static.serve, {
+    'document_root': settings.MEDIA_ROOT}),]
 urlpatterns += [
     path("", include(("apps.home.urls", "apps.home"), namespace="home"))
 ]  # Leave `Home.Urls` as the last
